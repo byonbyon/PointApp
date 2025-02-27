182b3d46d134905e2546267f24b99964727e8e3b
byonbyon <e.byonbyon@gmail.com>
2025-02-27 18:01:39 +0900

    신규

---
+++ MusinsaByonPoint/src/main/java/com/musinsa/byon/ByonPointApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.byon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByonPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByonPointApplication.class, args);
+	}
+
+}
---
+++ MusinsaByonPoint/src/main/java/com/musinsa/byon/controller/PointController.java
@@ -0,0 +1,12 @@
+package com.musinsa.byon.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PointController {
+
+	// 현재 포인트 조회
+	/*@GetMapping("/users/{userId}/points")
+	public String getPoints*/
+}
